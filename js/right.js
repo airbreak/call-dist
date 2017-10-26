c14995721230506623290e07fb491b874a06a8ca
airbreak <jiangjianming_m@163.com>
2017-10-27 00:47:53 +0800

    user contrlbox

--- js/right.js
+++ js/right.js
@@ -4,9 +4,6 @@ code=null;
 
 $(function(){
 
-
-
-
 	getEnvData();
 	var url=window.location.href;
 	// alert(url)
@@ -95,7 +92,7 @@ $(function(){
 
    })
 
-
+// initVideo();
 ctrlWidth();
 function ctrlWidth() {
   var width=$('.main-wrap').width()+'px',
@@ -176,5 +173,55 @@ function wxshare() {
 			}
 			/*vueInit();*/
 		});
+}
+
+
+function initVideo(){
+  // var player =$('video').mediaelementplayer();  
+
+  var player = new MediaElementPlayer($('#ideo1')[0],{
+    defaultVideoWidth: $('.main-wrap').width(),  
+    // if the <video height> is not specified, this is the default  
+    defaultVideoHeight: $('.main-wrap').height(),  
+    // if set, overrides <video width>  
+    videoWidth: -1,  
+    // if set, overrides <video height>  
+    videoHeight: -1,  
+    // width of audio player  
+    audioWidth: 400,  
+    // height of audio player  
+    audioHeight: 30,  
+    // initial volume when the player starts  
+    startVolume: 0.8,  
+    // useful for <audio> player loops  
+    loop: false,  
+    // enables Flash and Silverlight to resize to content size  
+    enableAutosize: true,  
+    // the order of controls you want on the control bar (and other plugins below)  
+    features: ['playpause','progress','current','duration','tracks','volume','fullscreen'],  
+    // Hide controls when playing and mouse is not over the video  
+    alwaysShowControls: false,  
+    // force iPad's native controls  
+    iPadUseNativeControls: false,  
+    // force iPhone's native controls  
+    iPhoneUseNativeControls: false,   
+    // force Android's native controls  
+    AndroidUseNativeControls: false,  
+    // forces the hour marker (##:00:00)  
+    alwaysShowHours: false,  
+    // show framecount in timecode (##:00:00:00)  
+    showTimecodeFrameCount: false,  
+    // used when showTimecodeFrameCount is set to true  
+    framesPerSecond: 25,  
+    // turns keyboard support on and off for this instance  
+    enableKeyboard: true,  
+    // when this player starts, it will pause other players  
+    pauseOtherPlayers: true,  
+    // array of keyboard commands  
+    keyActions: []  
+  });    // ... more code ...  
+  // player.pause();  
+  player.setSrc('big.mp4');  
+  player.play(); 
 }
 
